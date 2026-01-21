995700c37947d10350543ee9e62306cdd38f51d8
Muluken Zewge <mulukenzewge@gmail.com>
2026-01-21 12:13:24 +0300

    (fix):education title

--- src/components/About/AboutCard.jsx
+++ src/components/About/AboutCard.jsx
@@ -13,7 +13,7 @@ function AboutCard() {
            
             I am currently employed as a <span className="purple">Mobile Application developer </span>at <a href="https://2f-capital.com/" target="_blank" rel="noreferrer">Two F Capital</a>.
             <br />
-            I have completed BSc degree in <span className="purple">Computer Engineering</span>, followed by an intensive <span className="purple">Andvanced Algorithms and Software Development</span> education from the renowned <a href="https://a2sv.org/" target="_blank" rel="noreferrer">A2SV(Africa To Silicon Valley)</a>. 
+            I have completed BSc degree in <span className="purple">Computer Engineering</span>, followed by an intensive <span className="purple">Andvanced Algorithms and Software Engineering</span> education from the renowned <a href="https://a2sv.org/" target="_blank" rel="noreferrer">A2SV(Africa To Silicon Valley)</a>. 
             <br />
             
             Proficient in <span className="purple">Dart</span>, <span className="purple">Python</span>, and <span className="purple">Go</span>, I am always eager to learn new technologies and expand my skillset.
